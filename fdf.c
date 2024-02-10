b8d88baf306567d9b7b3b5b6c8b558ae163b7582
Artem Kozin <akozin@cbr9s1.42barcelona.com>
2024-02-10 16:39:26 +0100

    circle works

--- fdf.c
+++ fdf.c
@@ -6,12 +6,13 @@
 /*   By: akozin <akozin@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 15:13:03 by akozin            #+#    #+#             */
-/*   Updated: 2024/02/10 16:00:01 by akozin           ###   ########.fr       */
+/*   Updated: 2024/02/10 16:38:28 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include "mlx/mlx.h"
+#include <math.h>
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -19,6 +20,19 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel/8));
 	*(unsigned int *)dst = color;
+}
+
+/*
+double	dabs(double i)
+{
+	return (i * (i > 0));
+}
+*/
+int	circle_pixel_calc(int x, int y, double delta, double radius)
+{
+	return ((sqrt(pow(x - 1920.f/2, 2.f) + pow(y - 1080.f/2, 2.f)) -
+				radius <= delta) && (sqrt(pow(x - 1920.f/2, 2.f) + pow(y -
+				1080.f/2, 2.f)) - radius >= -delta));
 }
 
 int	main(void)
@@ -32,7 +46,12 @@ int	main(void)
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
 			&img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	for (int i = 0; i < 1920; i++)
+		for (int j = 0; j < 1080; j++)
+			{
+				my_mlx_pixel_put(&img, i, j,
+						0x00FF0000 * circle_pixel_calc(i, j, 9, 200));
+			}
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
